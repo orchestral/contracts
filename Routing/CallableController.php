5f8fd9eb068a331a2880ae73dc11847a245f37a9
crynobone <crynobone@gmail.com>
2014-10-15 16:36:53 +0800

    Add routing component.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Routing/CallableController.php
@@ -0,0 +1,13 @@
+<?php namespace Orchestra\Contracts\Routing;
+
+interface CallableController
+{
+    /**
+     * Execute an action on the controller.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function callAction($method, $parameters);
+}
---
+++ Routing/StackableController.php
@@ -0,0 +1,19 @@
+<?php namespace Orchestra\Contracts\Routing;
+
+interface StackableController
+{
+    /**
+     * Register middleware on the controller.
+     *
+     * @param  mixed  $middleware
+     * @return void
+     */
+    public function middleware($middleware);
+
+    /**
+     * Get the middleware assigned to the controller.
+     *
+     * @return array
+     */
+    public function getMiddleware();
+}
