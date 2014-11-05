2231f53900d3d489e4b9372e25f2029b60a0c9ff
crynobone <crynobone@gmail.com>
2014-11-05 21:51:50 +0800

    Add MessageBag contract.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Messages/MessageBag.php
@@ -0,0 +1,37 @@
+<?php namespace Orchestra\Contracts\Messages;
+
+use Closure;
+use Illuminate\Contracts\Support\MessageBag as MessageBagContract;
+
+interface MessageBag extends MessageBagContract
+{
+    /**
+     * Extend Messages instance from session.
+     *
+     * @param  \Closure $callback
+     * @return static
+     */
+    public function extend(Closure $callback);
+
+    /**
+     * Retrieve Message instance from Session, the data should be in
+     * serialize, so we need to unserialize it first.
+     *
+     * @return $this
+     */
+    public function retrieve();
+
+    /**
+     * Store current instance.
+     *
+     * @return void
+     */
+    public function save();
+
+    /**
+     * Compile the instance into serialize.
+     *
+     * @return string   serialize of this instance
+     */
+    public function serialize();
+}
