9f6bec885109e1f7e75a162f60ea25165b1b3f4d
crynobone <crynobone@gmail.com>
2014-11-29 21:07:50 +0800

    Move AuthenticateUser from foundation contract.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Auth/Command/AuthenticateUser.php
@@ -0,0 +1,23 @@
+<?php namespace Orchestra\Contracts\Auth\Command;
+
+use Orchestra\Contracts\Auth\Listener\AuthenticateUser as Listener;
+
+interface AuthenticateUser
+{
+    /**
+     * Login a user.
+     *
+     * @param  \Orchestra\Contracts\Auth\Listener\AuthenticateUser  $listener
+     * @param  array $input
+     * @return mixed
+     */
+    public function login(Listener $listener, array $input);
+
+    /**
+     * Logout a user.
+     *
+     * @param  \Orchestra\Contracts\Auth\Listener\AuthenticateUser  $listener
+     * @return mixed
+     */
+    public function logout(Listener $listener);
+}
