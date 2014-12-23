d18ff1d6db201eedcf729c12acebf198a0adbd5d
crynobone <crynobone@gmail.com>
2014-12-23 23:31:06 +0800

    Add PackageRepository contract.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Config/PackageRepository.php
@@ -0,0 +1,14 @@
+<?php namespace Orchestra\Contracts\Config;
+
+interface PackageRepository
+{
+    /**
+     * Register a package for cascading configuration.
+     *
+     * @param  string  $package
+     * @param  string  $hint
+     * @param  string  $namespace
+     * @return void
+     */
+    public function package($package, $hint, $namespace = null);
+}
