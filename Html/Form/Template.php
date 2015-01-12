a1da7cc204b64a0d04e00cee5815540d79792fdc
crynobone <crynobone@gmail.com>
2015-01-12 20:04:44 +0800

    Add Template contract for HTML Form.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Html/Form/Template.php
@@ -0,0 +1,76 @@
+<?php namespace Orchestra\Contracts\Html\Form;
+
+interface Template
+{
+    /**
+     * Button template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function button(Field $field);
+
+    /**
+     * Checkbox template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function checkbox(Field $field);
+
+    /**
+     * Checkboxes template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function checkboxes(Field $field);
+
+    /**
+     * File template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function file(Field $field);
+
+    /**
+     * Input template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function input(Field $field);
+
+    /**
+     * Password template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function password(Field $field);
+
+    /**
+     * Radio template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function radio(Field $field);
+
+    /**
+     * Select template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function select(Field $field);
+
+    /**
+     * Textarea template.
+     *
+     * @param  \Orchestra\Contracts\Html\Form\Field  $field
+     * @return string
+     */
+    public function textarea(Field $field);
+}
