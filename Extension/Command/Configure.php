ba975e138e7377f41600cfb3c1a17cd8d0663986
crynobone <crynobone@gmail.com>
2015-02-10 07:58:41 +0800

    Add extension command.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Extension/Command/Configure.php
@@ -0,0 +1,26 @@
+<?php namespace Orchestra\Contracts\Extension\Command;
+
+use Illuminate\Support\Fluent;
+use Orchestra\Contracts\Extension\Listener\Configure as Listener;
+
+interface Configure
+{
+    /**
+     * View edit extension configuration page.
+     *
+     * @param  \Orchestra\Contracts\Extension\Listener\Configure  $listener
+     * @param  \Illuminate\Support\Fluent  $extension
+     * @return mixed
+     */
+    public function configure(Listener $listener, Fluent $extension);
+
+    /**
+     * Update an extension configuration.
+     *
+     * @param  \Orchestra\Contracts\Extension\Listener\Configure  $listener
+     * @param  \Illuminate\Support\Fluent  $extension
+     * @param  array  $input
+     * @return mixed
+     */
+    public function update(Listener $listener, Fluent $extension, array $input);
+}
---
+++ Extension/Command/Viewer.php
@@ -0,0 +1,14 @@
+<?php namespace Orchestra\Contracts\Extension\Command;
+
+use Orchestra\Contracts\Extension\Listener\Viewer as Listener;
+
+interface Viewer
+{
+    /**
+     * View all extension page.
+     *
+     * @param  \Orchestra\Contracts\Extension\Listener\Viewer  $listener
+     * @return mixed
+     */
+    public function index(Listener $listener);
+}
