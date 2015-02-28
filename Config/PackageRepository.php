69697517345b7261cb14efc3b56eae3dc35dde0f
crynobone <crynobone@gmail.com>
2015-03-01 01:46:23 +0800

    Simplify condition to load package config.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- Config/PackageRepository.php
+++ Config/PackageRepository.php
@@ -3,14 +3,6 @@
 interface PackageRepository
 {
     /**
-     * Register a package with custom file.
-     *
-     * @param  string  $package
-     * @return void
-     */
-    public function file($package);
-
-    /**
      * Register a package for cascading configuration.
      *
      * @param  string  $package
