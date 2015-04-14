37c9f23f0d05130ab1de3450f7ad6815a467aef9
crynobone <crynobone@gmail.com>
2015-04-14 18:34:22 +0800

    Add additional contract to Orchestra\Contracts\Memory\Provider.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- Memory/Provider.php
+++ Memory/Provider.php
@@ -12,6 +12,34 @@ interface Provider extends DataContainer
     public function finish();
 
     /**
+     * Remove a item key.
+     *
+     * @param  string  $key
+     *
+     * @return void
+     */
+    public function forget($key);
+
+    /**
+     * Get a item value.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     *
+     * @return mixed
+     */
+    public function get($key, $default = null);
+
+    /**
+     * Check if item key has a value.
+     *
+     * @param  string  $key
+     *
+     * @return bool
+     */
+    public function has($key);
+
+    /**
      * Set a value from a key.
      *
      * @param  string  $key    A string of key to add the value.
