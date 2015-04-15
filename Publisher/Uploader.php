e12a491fa3413c7936af9ac47da83c4fb9d96c23
crynobone <crynobone@gmail.com>
2015-04-15 23:08:16 +0800

    Add Orchestra\Contracts\Publisher\Uploader.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Publisher/Uploader.php
@@ -0,0 +1,45 @@
+<?php namespace Orchestra\Contracts\Publisher; 
+
+interface Uploader
+{
+    /**
+     * Get service connection instance.
+     *
+     * @return self
+     */
+    public function getConnection();
+
+    /**
+     * Get service connection instance.
+     *
+     * @param  object  $client
+     *
+     * @return void
+     */
+    public function setConnection($client);
+
+    /**
+     * Connect to the service.
+     *
+     * @param  array  $config
+     *
+     * @return void
+     */
+    public function connect($config = []);
+
+    /**
+     * Upload the file.
+     *
+     * @param  string  $name
+     *
+     * @return bool
+     */
+    public function upload($name);
+
+    /**
+     * Verify that the driver is connected to a service.
+     *
+     * @return bool
+     */
+    public function connected();
+}
