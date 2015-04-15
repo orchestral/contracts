0c4754cf21b7c53423281109055a907b7896ae4c
crynobone <crynobone@gmail.com>
2015-04-16 07:30:08 +0800

    Add Orchestra\Contracts\Publisher\Publisher.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Publisher/Publisher.php
@@ -0,0 +1,20 @@
+<?php namespace Orchestra\Contracts\Publisher;
+
+interface Publisher 
+{
+    /**
+     * Publish extension.
+     *
+     * @param  string  $name
+     *
+     * @return void
+     */
+    public function extension($name);
+
+    /**
+     * Publish Orchestra Platform.
+     *
+     * @return void
+     */
+    public function foundation();
+}
