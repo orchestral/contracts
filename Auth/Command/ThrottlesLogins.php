1ba377b4abcb311491728b5ddee8ec1648c01cfe
crynobone <crynobone@gmail.com>
2015-07-04 00:06:29 +0800

    Working on Throttle contracts.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- Auth/Command/ThrottlesLogins.php
+++ Auth/Command/ThrottlesLogins.php
@@ -21,6 +21,15 @@ interface ThrottlesLogins
     public function getLoginAttempts(array $input);
 
     /**
+     * Get total seconds before doing another login attempts for the user.
+     *
+     * @param  array  $input
+     *
+     * @return int
+     */
+    public function getSecondsBeforeNextAttempts(array $input);
+
+    /**
      * Increment the login attempts for the user.
      *
      * @param  array  $input
@@ -28,4 +37,31 @@ interface ThrottlesLogins
      * @return int
      */
     public function incrementLoginAttempts(array $input);
+
+    /**
+     * Clear the login locks for the given user credentials.
+     *
+     * @param  array  $input
+     *
+     * @return void
+     */
+    public function clearLoginAttempts(array $input);
+
+    /**
+     * Get the login attempts cache key.
+     *
+     * @param  array  $input
+     *
+     * @return string
+     */
+    protected function getLoginAttemptsKey(array $input);
+
+     /**
+     * Get the login lock cache key.
+     *
+     * @param  array  $input
+     *
+     * @return string
+     */
+    protected function getLoginLockExpirationKey(array $input);
 }
--- Auth/Listener/ThrottlesLogins.php
+++ Auth/Listener/ThrottlesLogins.php
@@ -1,14 +1,16 @@
 <?php namespace Orchestra\Contracts\Auth\Listener;
 
-interface ThrottlesLogins extends AuthenticateUser
+use Orchestra\Contracts\Auth\Command\ThrottlesLogins as Command;
+
+interface ThrottlesLogins
 {
     /**
      * Redirect the user after determining they are locked out.
      *
      * @param  \Illuminate\Support\MessageBag|array  $errors
-     * @param  int  $seconds
+     * @param  \Orchestra\Contracts\Auth\Command\ThrottlesLogins|null  $throttles
      *
      * @return mixed
      */
-    public function sendLockoutResponse($errors, $seconds);
+    public function sendLockoutResponse($errors, Command $throttles = null);
 }
