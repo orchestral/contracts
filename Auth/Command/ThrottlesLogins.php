b015b144f42b996d6eba2eeb45a17c383a909de9
crynobone <crynobone@gmail.com>
2015-07-04 00:39:36 +0800

    Omit protected contract.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- Auth/Command/ThrottlesLogins.php
+++ Auth/Command/ThrottlesLogins.php
@@ -46,22 +46,4 @@ interface ThrottlesLogins
      * @return void
      */
     public function clearLoginAttempts(array $input);
-
-    /**
-     * Get the login attempts cache key.
-     *
-     * @param  array  $input
-     *
-     * @return string
-     */
-    protected function getLoginAttemptsKey(array $input);
-
-     /**
-     * Get the login lock cache key.
-     *
-     * @param  array  $input
-     *
-     * @return string
-     */
-    protected function getLoginLockExpirationKey(array $input);
 }
