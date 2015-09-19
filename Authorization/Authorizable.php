40fa88431e4fc2e23394683d3b8c53062cec16b1
crynobone <crynobone@gmail.com>
2015-09-19 22:23:13 +0800

    Authorizable doesn't need to extends Authenticatable to make it easier to extends outside of laravel app usage.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- Authorization/Authorizable.php
+++ Authorization/Authorizable.php
@@ -1,8 +1,6 @@
 <?php namespace Orchestra\Contracts\Authorization;
 
-use Illuminate\Contracts\Auth\Authenticatable;
-
-interface Authorizable extends Authenticatable
+interface Authorizable
 {
     /**
      * Get roles name as an array.
