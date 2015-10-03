39caa4ba0d3a4fe5b0f5d9498ebf088a12ff6e1c
crynobone <crynobone@gmail.com>
2015-10-03 14:17:27 +0800

    Add new `Orchestra\Contracts\Foundation\Application` contract.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Foundation/Application.php
@@ -0,0 +1,20 @@
+<?php namespace Orchestra\Contracts\Foundation;
+
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+
+interface Application extends ApplicationContract
+{
+    /**
+     * Get the path to the cached extension.json file.
+     *
+     * @return string
+     */
+    public function getCachedExtensionServicesPath();
+
+    /**
+     * Get the application's deferred services.
+     *
+     * @return array
+     */
+    public function getDeferredServices();
+}
