1d45d6fb9bafce8c3a25cf0ab9a99bd587396a41
crynobone <crynobone@gmail.com>
2016-05-29 13:39:52 +0800

    Add Specification contract.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Installation/Specification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Orchestra\Contracts\Installation;
+
+interface Specification
+{
+    /**
+     * Get specification id.
+     *
+     * @return string
+     */
+    public function uid();
+
+    /**
+     * Get specification title.
+     *
+     * @return string
+     */
+    public function title();
+
+    /**
+     * Get specification description.
+     *
+     * @return string
+     */
+    public function description();
+
+    /**
+     * Check specification requirement.
+     *
+     * @return bool
+     */
+    public function check();
+
+    /**
+     * Is specification optional.
+     *
+     * @return bool
+     */
+    public function optional();
+}
