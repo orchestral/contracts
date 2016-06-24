5cb58b7005b0b7318d35189c3e2aa9f3ec4c926d
crynobone <crynobone@gmail.com>
2016-06-24 14:08:59 +0800

    $control is always going to be an instance of Orchestra\Contracts\Html\Form\Field
    there's no reason to pass it as a parameter.
    
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Html/Form/Field.php
+++ src/Html/Form/Field.php
@@ -8,10 +8,9 @@ interface Field
      * Get value of column.
      *
      * @param  mixed  $row
-     * @param  mixed  $control
      * @param  array  $templates
      *
      * @return string
      */
-    public function getField($row, $control, array $templates = []);
+    public function getField($row, array $templates = []);
 }
