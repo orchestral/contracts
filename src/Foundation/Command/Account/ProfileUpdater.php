82dc832bdf771835fa94f8fa775fc2efc06c1d15
Mior Muhammad Zaki <crynobone@gmail.com>
2018-02-24 23:02:09 +0800

    Update to use Authenticatable.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Command/Account/ProfileUpdater.php
+++ src/Foundation/Command/Account/ProfileUpdater.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Contracts\Foundation\Command\Account;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Orchestra\Contracts\Foundation\Listener\Account\ProfileUpdater as Listener;
 
 interface ProfileUpdater
@@ -10,18 +11,20 @@ interface ProfileUpdater
      * Get account/profile information.
      *
      * @param  \Orchestra\Contracts\Foundation\Listener\Account\ProfileUpdater  $listener
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
      *
      * @return mixed
      */
-    public function edit(Listener $listener);
+    public function edit(Listener $listener, Authenticatable $user);
 
     /**
      * Update profile information.
      *
      * @param  \Orchestra\Contracts\Foundation\Listener\Account\ProfileUpdater  $listener
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
      * @param  array  $input
      *
      * @return mixed
      */
-    public function update(Listener $listener, array $input);
+    public function update(Listener $listener, Authenticatable $user, array $input);
 }
