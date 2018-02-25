042fe50e8af9862385f183bd8076a4c439a3aff0
Mior Muhammad Zaki <crynobone@gmail.com>
2018-02-25 10:44:43 +0800

    Revert.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Foundation/Command/Account/ProfileUpdater.php
+++ src/Foundation/Command/Account/ProfileUpdater.php
@@ -2,7 +2,6 @@
 
 namespace Orchestra\Contracts\Foundation\Command\Account;
 
-use Illuminate\Contracts\Auth\Authenticatable;
 use Orchestra\Contracts\Foundation\Listener\Account\ProfileUpdater as Listener;
 
 interface ProfileUpdater
@@ -11,20 +10,18 @@ interface ProfileUpdater
      * Get account/profile information.
      *
      * @param  \Orchestra\Contracts\Foundation\Listener\Account\ProfileUpdater  $listener
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
      *
      * @return mixed
      */
-    public function edit(Listener $listener, Authenticatable $user);
+    public function edit(Listener $listener);
 
     /**
      * Update profile information.
      *
      * @param  \Orchestra\Contracts\Foundation\Listener\Account\ProfileUpdater  $listener
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
      * @param  array  $input
      *
      * @return mixed
      */
-    public function update(Listener $listener, Authenticatable $user, array $input);
+    public function update(Listener $listener,array $input);
 }
